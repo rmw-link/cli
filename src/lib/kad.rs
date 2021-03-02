1b71e6b1c320cebe3598042eccfea41ab12a19f3
gcxfd <i@rmw.link>
2021-03-02 19:37:26 +0800

    🔶

--- src/lib/kad.rs
+++ src/lib/kad.rs
@@ -1,3 +1,4 @@
+use std::net::{SocketAddr,SocketAddrV4};
 use crate::var::ed25519::ED25519;
 use array_init::array_init;
 use std::cmp::PartialEq;
@@ -24,28 +25,19 @@ pub fn comm_bit_prefix(x: &[u8], y: &[u8]) -> usize {
 
 impl<'a, Addr: Debug + PartialEq + Copy, Socket> Kad<'a, Addr, Socket> {
   pub fn new(socket: &Socket) -> Kad<Addr, Socket> {
-    Kad {
+      Kad {
       socket,
       bucket: array_init(|_| VecDeque::new()),
     }
   }
   pub fn add(&mut self, pk: &[u8], addr: Addr) {
     // todo 测试是否是公网IP
-    println!(
-      "comm_bit_prefix {:?}",
-      comm_bit_prefix(&b"1230"[..], &b"1232"[..])
-    );
-    println!(
-      "comm_bit_prefix {:?}",
-      comm_bit_prefix(&b"112"[..], &b"7232"[..])
-    );
-    println!("comm_bit_prefix {:?}", comm_bit_prefix(pk, pk));
     let n = comm_bit_prefix(pk, ED25519.public.as_bytes());
     println!("comm_bit_prefix {:?}", n);
     let v = &mut self.bucket[n];
+
     if let None = v.iter().position(|&x| x == addr) {
       v.push_back(addr);
     }
-    println!("bucket {:?}", v)
   }
 }
