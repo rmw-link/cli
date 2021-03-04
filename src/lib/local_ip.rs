12b793f08716240839b8a750525c2b5162c5d2b0
gcxfd <i@rmw.link>
2021-03-04 12:19:03 +0800

    .

---
+++ src/lib/local_ip.rs
@@ -0,0 +1,20 @@
+use std::net::UdpSocket;
+
+/// get the local ip address, return an `Option<String>`. when it fail, return `None`.
+pub fn get() -> Option<String> {
+    let socket = match UdpSocket::bind("0.0.0.0:0") {
+        Ok(s) => s,
+        Err(_) => return None,
+    };
+
+    match socket.connect("119.29.29.29:53") {
+        Ok(()) => (),
+        Err(_) => return None,
+    };
+
+    match socket.local_addr() {
+        Ok(addr) => return Some(addr.ip().to_string()),
+        Err(_) => return None,
+    };
+}
+
